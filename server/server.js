e0a3d37064d7f22e3fc7081235d609e95e63322c
Manojav <manojav@hal.manojav.com>
2019-08-21 10:07:19 -0400

    formatting with VS code

--- server/server.js
+++ server/server.js
@@ -9,15 +9,15 @@ var app = express()
 const { Darknet } = require('darknet');
 
 const darknet = new Darknet({
-    weights: 'yolov3.weights',
-    config: 'yolov3.cfg',
-    namefile: 'coco.names'
+  weights: 'yolov3.weights',
+  config: 'yolov3.cfg',
+  namefile: 'coco.names'
 });
 
 const darknet_tiny = new Darknet({
-    weights: 'yolov3-tiny.weights',
-    config: 'yolov3-tiny.cfg',
-    namefile: 'coco.names'
+  weights: 'yolov3-tiny.weights',
+  config: 'yolov3-tiny.cfg',
+  namefile: 'coco.names'
 });
 
 app.post('/yolo', upload.single('photo'), function (req, res, next) {
@@ -25,24 +25,24 @@ app.post('/yolo', upload.single('photo'), function (req, res, next) {
   console.log(`-- yolo received --: ${filename}`)
   var predictions = "";
   try {
-	console.time("Detection");
-      predictions = darknet.detect(filename);
-	console.timeEnd("Detection");
+    console.time("Detection");
+    predictions = darknet.detect(filename);
+    console.timeEnd("Detection");
 
-}  catch (err)  {
-  console.log(`-- yolo: --:${err.message}`);
-} 
+  } catch (err) {
+    console.log(`-- yolo: --:${err.message}`);
+  }
   var result = "";
 
-predictions.forEach(function (prediction) {
-	
-  	result += prediction.name + ":" + prediction.prob.toFixed(2) + ","; 
+  predictions.forEach(function (prediction) {
+
+    result += prediction.name + ":" + prediction.prob.toFixed(2) + ",";
 
 
-});
-	console.log(`-- yolo: predictions --: ${result}`);
+  });
+  console.log(`-- yolo: predictions --: ${result}`);
   res.json(predictions);
-  fs.unlink(filename, d => {})
+  fs.unlink(filename, d => { })
   console.log(`-- yolo processed  --: ${filename}`)
 
 })
@@ -52,26 +52,26 @@ app.post('/yolo-tiny', upload.single('photo'), function (req, res, next) {
   var filename = `./${req.file.destination}${req.file.filename}`
   console.log(`-- yolo-tiny received --: ${filename}`)
   var predictions = "";
-  try { 
-	console.time("Detection");
-      predictions = darknet_tiny.detect(filename);
-	console.timeEnd("Detection");
+  try {
+    console.time("Detection");
+    predictions = darknet_tiny.detect(filename);
+    console.timeEnd("Detection");
 
-}  catch (err)  {
-  console.log(`-- yolo-tiny: --:${err.message}`);
-}
+  } catch (err) {
+    console.log(`-- yolo-tiny: --:${err.message}`);
+  }
   var result = "";
-  
-predictions.forEach(function (prediction) {
 
-        result += prediction.name + ":" + prediction.prob.toFixed(2) + ",";
+  predictions.forEach(function (prediction) {
+
+    result += prediction.name + ":" + prediction.prob.toFixed(2) + ",";
 
 
-});
+  });
 
-	console.log(`-- yolo: predictions --: ${result}`);
+  console.log(`-- yolo: predictions --: ${result}`);
   res.json(predictions);
-  fs.unlink(filename, d => {})
+  fs.unlink(filename, d => { })
   console.log(`-- yolo-tiny processed  --: ${filename}`)
 
 })
